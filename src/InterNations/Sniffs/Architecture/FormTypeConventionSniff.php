f36f1dc0fdd10d531344a0074ddee1dfb6d54007
Lars Strojny <lars.strojny@internations.org>
2014-10-15 23:51:41 +0200

    Fixing regex

--- src/InterNations/Sniffs/Architecture/FormTypeConventionSniff.php
+++ src/InterNations/Sniffs/Architecture/FormTypeConventionSniff.php
@@ -22,7 +22,7 @@ class FormTypeConventionSniff implements CodeSnifferSniff
         }
 
         $className = $file->getDeclarationName($stackPtr);
-        if (!preg_match('/.*FormType/', $className)) {
+        if (!preg_match('/.*FormType$/D', $className)) {
             $file->addError(
                 sprintf('Form types are expected to be named "<Something>FormType", "%s" found', $className),
                 $stackPtr,
