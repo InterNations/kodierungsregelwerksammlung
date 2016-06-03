c484318346ec6bb3204b06f8a39871155c6f4e6f
Lars Strojny <lars@strojny.net>
2016-06-03 20:00:36 +0200

    Add sniff for empty format string

---
+++ tests/InterNations/Tests/Sniffs/Waste/Fixtures/SuperfluousFormatString/Functions.php
@@ -0,0 +1,5 @@
+<?php
+printf('foo');
+sprintf('foo' . 'bar');
+sprintf("foo");
+sprintf('%s', 'foo');
---
+++ tests/InterNations/Tests/Sniffs/Waste/SuperfluousFormatStringSniffTest.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../AbstractTestCase.php';
+
+class InterNations_Tests_Sniffs_Waste_SuperfluousFormatStringSniffTest extends InterNations_Tests_Sniffs_AbstractTestCase
+{
+    public function testSuperfluousFormatStrings()
+    {
+        $file = __DIR__ . '/Fixtures/SuperfluousFormatString/Functions.php';
+        $errors = $this->analyze(['InterNations/Sniffs/Waste/SuperfluousFormatStringSniff'], [$file]);
+
+        $this->assertReportCount(3, 0, $errors, $file);
+        $this->assertReportContains(
+            $errors,
+            $file,
+            'errors',
+            'Superfluous printf() call as no parameters are passed'
+        );
+        $this->assertReportContains(
+            $errors,
+            $file,
+            'errors',
+            'Superfluous sprintf() call as no parameters are passed'
+        );
+        $this->assertReportContains(
+            $errors,
+            $file,
+            'errors',
+            'Superfluous sprintf() call as no parameters are passed'
+        );
+    }
+}
