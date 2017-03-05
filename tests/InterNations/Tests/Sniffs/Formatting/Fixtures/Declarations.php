5189a874a20a25a934bd1d617d93040fc944a3bd
Egidijus Lukauskas <egils.ps@gmail.com>
2017-03-05 16:19:33 +0200

    Consider return type as part of an expression for function declarations (#30)
    * Consider return type as part of an expression for function declarations
    
    Return type was not considered to be a part of an expression when
    validating multi-line expression elngth
    
    * Wrong selection of pointer lead to failing tests
    
    * And it's time to fix the phpcs

--- tests/InterNations/Tests/Sniffs/Formatting/Fixtures/Declarations.php
+++ tests/InterNations/Tests/Sniffs/Formatting/Fixtures/Declarations.php
@@ -16,3 +16,28 @@ class Test
 
     }
 }
+
+class TestTwo
+{
+    public function createTest(
+        array $fakeVariable,
+        int $secondFakeArgument,
+        string $thirdFakeArgument,
+        bool $false
+    ): Test
+    {
+        return new Test();
+    }
+}
+
+class TestThree
+{
+    public function createTest(
+        array $fakeVariable,
+        int $secondFakeArgument,
+        string $thirdFakeArgument
+    ): Test
+    {
+        return new Test();
+    }
+}
