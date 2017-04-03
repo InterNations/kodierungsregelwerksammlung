c9dcd0e6ca9189799d138a80b3f011e032113c07
Malvika Chauhan <malvika.chauhan@internations.org>
2017-04-03 14:37:58 +0200

    Enforce type hints for new/modified code

--- tests/InterNations/Tests/Sniffs/Syntax/Fixtures/MethodTypeHints/SuperfluousParamDoc.php
+++ tests/InterNations/Tests/Sniffs/Syntax/Fixtures/MethodTypeHints/SuperfluousParamDoc.php
@@ -1,10 +1,6 @@
 <?php
 
 class superfluousParamDoc
-<<<<<<< HEAD
-=======
-
->>>>>>> Enforce type hints for new/modified code
 {
     /**
      * @param Activity $activity
@@ -12,32 +8,6 @@ class superfluousParamDoc
      */
     public function accept(Activity $activity, User $attendee): Attendance
     {
-<<<<<<< HEAD
         return;
-=======
-        if ($this->accessResolver->resolve($activity->getGuestlist(), $attendee)->canAttendWithTrial()) {
-            $attendance = $this->attendanceService->acceptUsingTrial($activity->getGuestlist(), $attendee);
-
-            $this->dispatcher->dispatch(
-                ActivityGroupEvents::onActivityAttendanceAccept,
-                AttendanceEvent::createTrial($attendance, $activity)
-            );
-
-            return $attendance;
-        }
-
-        if ($this->needsToJoinGroup($activity->getActivityGroup(), $attendee)) {
-            throw LogicException::mustJoinGroup();
-        }
-
-        $attendance = $this->attendanceService->accept($activity->getGuestlist(), $attendee);
-
-        $this->dispatcher->dispatch(
-            ActivityGroupEvents::onActivityAttendanceAccept,
-            AttendanceEvent::create($attendance, $activity)
-        );
-
-        return $attendance;
->>>>>>> Enforce type hints for new/modified code
     }
 }
